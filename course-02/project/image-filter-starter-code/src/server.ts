e470958567b1a84c76a273cd5cc15fa1e17e94bd
Stoyan Halev <sto@foryouandyourcustomers.com>
2020-04-17 23:06:11 +0300

    EB project submission

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -1,45 +1,59 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import {deleteLocalFiles, filterImageFromURL} from './util/util';
 
 (async () => {
 
-  // Init the Express application
-  const app = express();
+    // Init the Express application
+    const app = express();
 
-  // Set the network port
-  const port = process.env.PORT || 8082;
-  
-  // Use the body parser middleware for post requests
-  app.use(bodyParser.json());
+    // Set the network port
+    const port = process.env.PORT || 8080;
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+    // Use the body parser middleware for post requests
+    app.use(bodyParser.json());
 
-  /**************************************************************************** */
+    /**************************************************************************** */
+    app.get("/filteredimage", async (req, res) => {
+        let {image_url} = req.query;
 
-  //! END @TODO1
-  
-  // Root Endpoint
-  // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
-    res.send("try GET /filteredimage?image_url={{}}")
-  } );
-  
+        // Check if there is an image
+        if (!image_url) {
+            return res.status(400).send("Please provide image_url as query parameter");
+        }
 
-  // Start the Server
-  app.listen( port, () => {
-      console.log( `server running http://localhost:${ port }` );
-      console.log( `press CTRL+C to stop server` );
-  } );
+        // If it's not an image, we stop the request to go further
+        // This can lead to problems with redirecting urls and unsupported file format
+        if (!image_url.match(/\.(jpeg|jpg|gif|png|svg)$/)) {
+            return res.status(400).send("Please provide image_url with the following formats: jpeg,jpg,gif,png,svg");
+        }
+
+        let filterImagePath: string;
+        try {
+            filterImagePath = await filterImageFromURL(image_url);
+            res.status(200).sendFile(filterImagePath)
+        } catch (err) {
+            res.status(422).send("Please provide correct image url");
+        } finally {
+            if (filterImagePath) {
+                console.log("Deleting files...");
+                res.on('finish', () => deleteLocalFiles([filterImagePath]));
+            }
+        }
+    });
+
+    //! END @TODO1
+
+    // Root Endpoint
+    // Displays a simple message to the user
+    app.get("/", async (req, res) => {
+        res.send("try GET /filteredimage?image_url=https://upload.wikimedia.org/wikipedia/commons/7/75/Cute_grey_kitten.jpg")
+    });
+
+
+    // Start the Server
+    app.listen(port, () => {
+        console.log(`server running http://localhost:${port}`);
+        console.log(`press CTRL+C to stop server`);
+    });
 })();
